9cdf37d43cd8bb282a3932caac8f3b0d5e33e1f2
Farsana <farsanavs786441@gmail.com>
2026-03-19 15:55:26 +0530

    profile scetion added

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -84,7 +84,7 @@ const ProfileScreen: React.FC = () => {
                         <Image source={require('../../assets/icons/profile/cart.png')} style={{ width: 22, height: 22 }} />
                         <Text style={styles.actionText}>Cart</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.actionCard} onPress={() => navigation.navigate('MyOrder')}>
+                    <TouchableOpacity style={styles.actionCard}>
                         <Image source={require('../../assets/icons/profile/orders.png')} style={{ width: 22, height: 22 }} />
                         <Text style={styles.actionText}>My Order</Text>
                     </TouchableOpacity>
